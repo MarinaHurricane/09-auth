05c7cd162d0501cf98c5eaae69eb4c2e453d3c1c
MarinaHurricane <maryishch88@gmail.com>
2026-03-04 17:18:38 +0000

    edit profile

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { fetchNoteById } from "@/lib/api";
+import { fetchNoteById } from "@/lib/clientApi";
 import { useQuery } from "@tanstack/react-query";
 import css from "./NoteDetails.module.css";
 
